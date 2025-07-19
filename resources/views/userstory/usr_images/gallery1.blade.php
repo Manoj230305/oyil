a08cce0002cabb172cb43339cd6ef4db90744336
hooman <hooman@beingggggggggggggg.com>
2025-07-19 22:34:26 +0530

    Rename of content based on user

---
+++ resources/views/userstory/usr_images/gallery1.blade.php
@@ -0,0 +1,215 @@
+<section class="img-gallery section-padding" id="portfolio" data-scroll-index="3">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="section-title">
+                        <!-- <h4>Portfolio</h4> -->
+                        <h2>Featured <span>Photography</span> Moments</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row ">
+                <div class="col-lg-8 d-flex justify-content-center sortBtn flex-wrap">
+                    <a href="#" class="filter-btn active" data-filter="*">All</a>
+                    <a href="#" class="filter-btn " data-filter=".wordpress">Pre-Wedding</a>
+                    <a href="#" class="filter-btn " data-filter=".photoshop">Baby Shower</a>
+                    <a href="#" class="filter-btn " data-filter=".web-design">Potraits</a>
+                </div>
+            </div>
+
+            <div class="row grid">
+                <div class="col-lg-4 col-md-6 col-sm-6 wordpress">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/7.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/7.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 photoshop">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/11.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/11.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 wordpress">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/23.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/23.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 web-design">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/26.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/26.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 photoshop">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/9.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/9.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 photoshop">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/12.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/12.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 wordpress">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/7.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/7.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 photoshop">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/11.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/11.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 wordpress">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/23.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/23.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 web-design">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/26.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/26.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 photoshop">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/9.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/9.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6 photoshop">
+                    <div class="single-work text-center mt-30">
+                        <div class="work-image">
+                            <img src="{{ asset('images/12.JPG') }}" alt="portfolio image">
+                        </div>
+
+                        <div class="work-overlay">
+                            <div class="work-content">
+                                <ul>
+                                    <li><a href="{{ asset('images/12.JPG') }}" class="image-popup"><i class="fa-solid fa-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
